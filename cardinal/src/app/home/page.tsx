3a9dcd9ce0c975fbfdb17885b27003adf5e878b8
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2023-10-28 19:38:24 -0500

    updated home page

--- cardinal/src/app/home/page.tsx
+++ cardinal/src/app/home/page.tsx
@@ -1,7 +1,21 @@
-export default function HomePage(){
-    return (
-        <div>
-            
-        </div>
-    );
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Head>
+        <title>My Medical App</title>
+        <meta name="description" content="Manage your medical history and medication" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="bg-white p-10 rounded-lg shadow-md w-full max-w-md">
+        <h1 className="text-3xl mb-4 font-semibold text-gray-700">Welcome to My Medical App</h1>
+        <p className="text-gray-600 mb-6">Your one-stop solution for medical history and medication management.</p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-200">
+          Login
+        </button>
+      </div>
+    </div>
+  )
 }
